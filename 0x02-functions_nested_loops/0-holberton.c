bf03769a27d8cc5631d7815a4d99ab0219c1404d
Iantechie <77444698+Iantechie@users.noreply.github.com>
2021-06-24 00:12:24 +0300

    Functions nested loops

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,17 +1,17 @@
 #include "holberton.h"
 /**
- * main - Entry point
+ *main - Entry point
  *
- * Return: nothing, return void
+ *Return: nothing, return void
  */
 int main(void)
 {
-int i; /*Declaring statements*/
-char string[9] = "Holberton"; /*Arrays*/
-for (i = 0 ; i < 9 ; i++) /*Start for*/
+int i;/*Declaring statements*/
+char string[9] = "Holberton";/*Arrays*/
+for (i = 0 ; i < 9 ; i++)/*Start for*/
 {
 _putchar(string[i]);/*display word*/
-} /*End for*/
+}/*End for*/
 _putchar('\n');
 return (0);
 }
