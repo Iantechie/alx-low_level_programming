16c5f603ddd683dc68c049bed3ce665d3a1ecf1b
Iantechie <kipronoian68@gmail.com>
2021-06-28 04:59:48 -0700

    Pointers arrays and strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,8 @@
+#include <stdioh.h>
+#include "holberton.h"
+/**
+ * put2- function that prints every other character in a string
+ * @str- value to be evaluated
+ * Return not
+ */
+
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,10 @@
+#include <stdioh>
+#include "holberton"
+/**
+ * puts_half - function that prints half of a string
+ * @str- value to be evaluated
+ * Return: not
+ */
+void puts_half(char *str)
+	{
+	
