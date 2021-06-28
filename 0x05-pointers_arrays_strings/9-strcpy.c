7c6bd0240cec4e614854b6c4ed0bfce7f1f66246
Iantechie <kipronoian68@gmail.com>
2021-06-28 12:01:13 -0700

    Pointers arrays and strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * strcpy- function that copies a str pointed to by src
+ * @dest- value to be evaluated
+ * @str- value to be evaluated
+ * Return: not
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int x = 0;
+	int l = 0;
+
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for (; x < l ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
+	return (dest);
+}
