9acbaa548c2c8b4f0df18a9f8f6c133db94cd9bf
Iantechie <kipronoian68@gmail.com>
2021-06-28 12:19:46 -0700

    Pointers arrays and strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 #include "holberton.h"
 /**
  * puts2- function that prints every other character in a string
- * @str- value to be evaluated
+ * @str: value to be evaluated
  * Return not
  */
 void puts2(char *str)
